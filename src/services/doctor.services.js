858edca340b37987751cca0494d70aaea75ad225
JefferGonzalez <jefferd2016@gmail.com>
2022-12-06 13:04:43 -0500

    Add services for doctors

---
+++ src/services/doctor.services.js
@@ -0,0 +1,24 @@
+import Doctor from "./../models/Doctor.js";
+
+export const findAll = async () => {
+  return await Doctor.find();
+};
+
+export const findById = (id) => {
+  return Doctor.findById(id);
+};
+
+export const create = async (doctor) => {
+  const newDoctor = await new Doctor(doctor).save();
+  return newDoctor;
+};
+
+export const updateById = async (_id, data) => {
+  const doctor = Doctor.findByIdAndUpdate(_id, data);
+  if (doctor) return true;
+  return false;
+};
+
+export const deleteById = async (_id) => {
+  await Doctor.findByIdAndDelete(_id);
+};
