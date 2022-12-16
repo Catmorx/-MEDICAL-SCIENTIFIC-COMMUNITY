7205fccafa7a2a9a67e1d9384734b6d378771141
titol2003 <titolospina@gmail.com>
2022-12-16 18:25:11 -0500

    Ultiva version (Login & Register sirven)

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import login from "../controllers/login.controller.js";
-import { createPaciente } from "../controllers/paciente.controller";
+import { createPaciente } from "../controllers/paciente.controller.js";
 
 const loginRouter = express.Router();
 
